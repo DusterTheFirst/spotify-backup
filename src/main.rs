18a7b6c603426c9fe8737e2c7068b1461086f79b
Zachary <me@dusterthefirst.com>
2021-10-21 19:39:34 +0200

    rust 2021

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-use std::{convert::TryInto, str::FromStr};
+use std::{env, str::FromStr};
 
 use anyhow::Context;
-use async_std::{
-    stream::{Stream, StreamExt},
-    task::block_on,
-};
+use async_std::{stream::StreamExt, task::block_on};
 use rspotify::{
-    clients::BaseClient,
-    model::{SearchType, UserId},
-    ClientCredsSpotify, Credentials,
+    clients::{BaseClient, OAuthClient},
+    model::{SimplifiedPlaylist, UserId},
+    scopes, AuthCodeSpotify, ClientCredsSpotify, Credentials, OAuth,
 };
 
 fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().ok();
+    let ci = env::var("CI").is_ok();
 
     tracing_subscriber::fmt().init();
 
     let creds = Credentials::from_env().context("No rspotify credentials")?;
+    let oauth = OAuth {
+        scopes: scopes!("playlist-read-private"),
+        redirect_uri: "http://localhost:8080/callback".into(),
+        ..Default::default()
+    };
 
-    let mut spotify = ClientCredsSpotify::new(creds);
+    let mut spotify = AuthCodeSpotify::new(creds, oauth);
 
     block_on::<_, anyhow::Result<()>>(async {
-        spotify.request_token().await?;
+        spotify
+            .prompt_for_token(&spotify.get_authorize_url(false)?)
+            .await?;
 
         let user_id = UserId::from_str("dusterthefirst")?;
 
         let mut playlists = spotify.user_playlists(&user_id);
 
-        while let Some(playlist) = playlists.next().await.transpose()? {
+        while let Some(playlist) = playlists.next().await {
+            let playlist: SimplifiedPlaylist = playlist?;
+
             dbg!(playlist.name);
         }
 
