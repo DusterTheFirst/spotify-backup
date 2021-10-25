cdef58640ec5a3cd1d3d779acd70557f6e501e89
Zachary <me@dusterthefirst.com>
2021-10-25 12:40:50 +0200

    setup serverless

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use lambda_runtime::{handler_fn, Context, Error};
+use serde_json::Value;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(handler_fn(handler)).await?;
+    Ok(())
+}
+
+async fn handler(event: Value, _ctx: Context) -> Result<Value, Error> {
+    Ok(event)
+}
