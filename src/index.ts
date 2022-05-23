2d0af5c6f6981a733b2f1f568659e7454adfd225
Zachary <me@dusterthefirst.com>
2022-05-23 09:54:29 +0200

    Authentication URL

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ interface SpotifyKV {
     readonly refresh_token: string;
 }
 
+const SPOTIFY_API = "https://api.spotify.com/";
+const SPOTIFY_ACCOUNTS = "https://accounts.spotify.com/";
+
 function html_response(body: string, head: string = "") {
     return new Response(
         `<!DOCTYPE html><head>${head}</head><body>${body} </body>
@@ -24,50 +27,53 @@ function html_response(body: string, head: string = "") {
     );
 }
 
-async function fetch_home(spotify_token: SpotifyKV | null) {
+async function fetch_home(spotify_token: SpotifyKV | null, auth_url: URL) {
     if (spotify_token !== null) {
         return html_response(
-            `user already authenticated<br/><a href="/start-auth">Re-authenticate</a>`
+            `user already authenticated<br/><a href="${auth_url}">Re-authenticate</a>`
         );
     }
 
     return html_response(
-        `user not authenticated<br/><a href="/start-auth">Authenticate</a>`
+        `user not authenticated<br/><a href="${auth_url}">Authenticate</a>`
     );
 }
 
+function create_authentication_url(env: Environment, origin: string): URL {
+    let auth_url = new URL(SPOTIFY_ACCOUNTS);
+    auth_url.pathname = "/authorize";
+    auth_url.searchParams.append("client_id", env.SPOTIFY_CLIENT_ID);
+    auth_url.searchParams.append("response_type", "code");
+    auth_url.searchParams.append("redirect_uri", `${origin}/auth`);
+    auth_url.searchParams.append("scope", "playlist-read-private");
+    auth_url.searchParams.append("show_dialog", "false");
+    return auth_url;
+}
+
 async function authenticate_spotify(
-    client_id: string,
-    client_secret: string
+    env: Environment,
+    origin: string
 ): Promise<Response> {
-    return Response.redirect("https://com", 307);
+    return Response.redirect(origin, 307);
 }
 
 export default {
-    async fetch(
-        request: Request,
-        {
-            SPOTIFY_BACKUP_KV,
-            SPOTIFY_CLIENT_ID,
-            SPOTIFY_CLIENT_SECRET,
-        }: Environment
-    ): Promise<Response> {
-        let spotify_token = await SPOTIFY_BACKUP_KV.get<SpotifyKV>(
+    async fetch(request: Request, env: Environment): Promise<Response> {
+        let spotify_token = await env.SPOTIFY_BACKUP_KV.get<SpotifyKV>(
             SPOTIFY_KV_TOKEN,
             "json"
         );
 
         let url = new URL(request.url);
+        let origin = url.origin;
+        let auth_url = create_authentication_url(env, origin);
 
         if (request.method === "GET") {
             switch (url.pathname) {
                 case "/":
-                    return await fetch_home(spotify_token);
-                case "/start-auth":
-                    return await authenticate_spotify(
-                        SPOTIFY_CLIENT_ID,
-                        SPOTIFY_CLIENT_SECRET
-                    );
+                    return await fetch_home(spotify_token, auth_url);
+                case "/auth":
+                    return await authenticate_spotify(env, origin);
                 default:
                     return new Response(`route not found`, { status: 404 });
             }
@@ -75,15 +81,8 @@ export default {
             return new Response(`unexpected request method`, { status: 400 });
         }
     },
-    async scheduled(
-        event: ScheduledEvent,
-        {
-            SPOTIFY_BACKUP_KV,
-            SPOTIFY_CLIENT_ID,
-            SPOTIFY_CLIENT_SECRET,
-        }: Environment
-    ) {
-        let spotify_token = await SPOTIFY_BACKUP_KV.get<SpotifyKV>(
+    async scheduled(event: ScheduledEvent, env: Environment) {
+        let spotify_token = await env.SPOTIFY_BACKUP_KV.get<SpotifyKV>(
             SPOTIFY_KV_TOKEN,
             "json"
         );
@@ -95,8 +94,8 @@ export default {
 
         const client = new spotify.Client({
             token: {
-                clientID: SPOTIFY_CLIENT_ID,
-                clientSecret: SPOTIFY_CLIENT_SECRET,
+                clientID: env.SPOTIFY_CLIENT_ID,
+                clientSecret: env.SPOTIFY_CLIENT_SECRET,
                 token: spotify_token.token,
                 refreshToken: spotify_token.refresh_token,
             },
