57a71c8e9d8a28a543c423f3ee1828241a678ef8
Zachary Kohnen <me@dusterthefirst.com>
2023-04-15 22:44:30 +0200

    fix redirect to domain middleware to use relative uri
    previous implementation panicked due to no scheme set

--- src/router/middleware/redirect.rs
+++ src/router/middleware/redirect.rs
@@ -1,7 +1,10 @@
 use axum::{
     body::{Body, BoxBody},
     extract::{Host, State},
-    http::{uri::Authority, Request, Uri},
+    http::{
+        uri::{Authority, Scheme},
+        Request, Uri,
+    },
     middleware::Next,
     response::{IntoResponse, Redirect, Response},
 };
@@ -18,14 +21,10 @@ pub async fn redirect_to_domain(
     } else {
         trace_span!("redirect_to_domain", ?domain, %hostname).in_scope(|| {
             // Inherit path and query from request
-            let mut parts = req.uri().clone().into_parts();
-            parts.authority = Some(domain);
-
             trace!("URI authority did not match configured DOMAIN");
 
-            let uri = Uri::from_parts(parts).expect("redirect URI should be a valid URI");
-
-            Redirect::permanent(&uri.to_string()).into_response()
+            Redirect::permanent(&format!("//{}{}", domain.as_str(), req.uri().path()))
+                .into_response()
         })
     }
 }
